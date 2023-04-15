9af8f8e10a29ac6f910c7f44c7ae491daa14bf15
YuriiAlekseev <ash030788@gmail.com>
2023-04-15 14:28:03 +0300

    Добавление метода toString

--- Employee.java
+++ Employee.java
@@ -53,6 +53,10 @@ public class Employee{
     public int hashCode() {
         return Objects.hash(firstName, lastName);
     }
+    @Override
+    public String toString(){
+        return firstName + " " + lastName;
+    }
 
     public String getFullName() {
         return firstName + " " + lastName;
